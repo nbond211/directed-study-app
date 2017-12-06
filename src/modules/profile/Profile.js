c8b4c60a903b81656b0b2c1e04122052a4984b80
Nick Bond <bond.n@husky.neu.edu>
2017-12-05 22:28:35 -0500

    add header to profile screen

--- src/modules/profile/Profile.js
+++ src/modules/profile/Profile.js
@@ -1,29 +1,46 @@
 import React from 'react';
-import { Container, Header, Tab, Tabs, ScrollableTab, Icon } from 'native-base';
+import { Container, Header, Left, Body, Right, Title, Icon } from 'native-base';
+import { Column, Row, Grid } from 'react-native-easy-grid';
 
 import Styles from './styles';
 import Text from '../../components/text';
 
-const Profile = () => {
+class Profile extends React.Component {
+    static displayName = 'Alex';
 
-    return (
-        <Container>
-            <Text>Profile</Text>
-        </Container>
-    )
-};
+    static navigationOptions = {
+        tabBarLabel: 'Alex',
+        tabBarIcon: ({ tintColor }) => (
+            <Icon
+                name='md-person'
+                size={26}
+                style={{ color: tintColor }}
+            />
+        )
+    };
 
-Profile.displayName = 'Alex';
+    render() {
+        return (
+            <Container>
+                <Header style={Styles.header}>
+                    <Left/>
+                    <Body>
+                    <Text style={Styles.headerText}>Hi, Alex!</Text>
+                    </Body>
+                    <Right />
+                </Header>
+                <Grid>
+                    <Row>
+                        <Text>Notifications</Text>
+                    </Row>
+                    <Row>
+                        <Text>How often do you want to participate in a challenge?</Text>
+                    </Row>
+                </Grid>
+            </Container>
+        );
+    }
+}
 
-Profile.navigationOptions = {
-    tabBarLabel: 'Alex',
-    tabBarIcon: ({ tintColor }) => (
-        <Icon
-            name='md-person'
-            size={26}
-            style={{ color: tintColor }}
-        />
-    )
-};
 
 export default Profile;
---
+++ src/modules/profile/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const white = '#FFF';
+const purple ='#7875FF';
+const lightPurple = '#ABAFFC';
+
+export default StyleSheet.create({
+    header: {
+        backgroundColor: purple
+    },
+    headerText: {
+        color: white,
+        fontSize: 15
+    }
+});
